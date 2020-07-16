c6444bccee9cd622abc99b2cbecd7a0be02f4ec6
sprajjwal <shashwatprajjwal@gmail.com>
2020-07-16 09:39:39 -0700

    map

--- views/myApp/map/index.js
+++ views/myApp/map/index.js
@@ -14,7 +14,13 @@ export default class Map extends Component {
   render() {
     return(
       <View style={styles.view}>
-        <MapView/>
+        <MapView style={styles.mapStyle} 
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}/>
       </View>
     )
   }
